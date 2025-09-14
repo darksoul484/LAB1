4bf9d7e1e7d771ac557a850b0d6b57f56a93ebbc
darksoul484 <mmahjabin484@gmail.com>
2025-09-14 15:31:39 +0600

    First upload

---
+++ project.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n <= 1) return false;      
+    if (n == 2) return true;     
+    if (n % 2 == 0) return false;  
+
+    for (int i = 3; i <= sqrt(n); i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    if (isPrime(num))
+        cout << num << " is a Prime number." << endl;
+    else
+        cout << num << " is NOT a Prime number." << endl;
+
+    return 0;
+}
